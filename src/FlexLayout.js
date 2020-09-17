c54dcd9c054c2fb3a7b69c0b93b5da3b74218500
trycrmr <trycrmr@gmail.com>
2020-09-17 13:59:49 -0400

    Positionally styles flex layout

--- src/FlexLayout.js
+++ src/FlexLayout.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./FlexLayout.css";
 
 const FlexLayout = (props) => {
-  return <>FlexLayout</>;
+  return (
+    <section className="flex-layout">
+      {Array(10)
+        .fill("test div")
+        .map((thisDiv) => (
+          <div className="flex-children">{thisDiv}</div>
+        ))}
+    </section>
+  );
 };
 
 export default FlexLayout;
