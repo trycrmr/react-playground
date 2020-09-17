55a8faa27d9b875b58625d141ad37f06e8249e3f
trycrmr <trycrmr@gmail.com>
2020-09-17 11:22:37 -0400

    Inits layout

---
+++ src/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Layout.css";
+const Layout = () => {
+  return (
+    <div>
+      Layout yeet
+      {/* <PageContainer>
+      <Header>
+        <Nav />
+        <Main />
+        <Footer />
+      </Header>
+    </PageContainer> */}
+    </div>
+  );
+};
+
+export default Layout;
